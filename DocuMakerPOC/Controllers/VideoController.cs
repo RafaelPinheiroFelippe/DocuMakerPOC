f2c663068b4ec62802cd205980550ac519f77c12
Rafael Pinheiro Felippe <rafafe23@gmail.com>
2023-07-27 00:47:12 -0300

    chore: Gather token from appsettings

--- DocuMakerPOC/Controllers/VideoController.cs
+++ DocuMakerPOC/Controllers/VideoController.cs
@@ -7,10 +7,17 @@ namespace DocuMakerPOC.Controllers;
 [Route("Video")]
 public class VideoController : ControllerBase
 {
+    private readonly GenerateC4Script _generateC4Script;
+
+    public VideoController(GenerateC4Script generateC4Script)
+    {
+        _generateC4Script = generateC4Script;
+    }
+
     [HttpPost("{videoPath}", Name = "Vid2C4")]
     public async Task<IActionResult> Vid2C4(string videoPath)
     {
-        return await GenerateC4Script.RunAsync(videoPath)
+        return await _generateC4Script.RunAsync(videoPath)
             ? Ok()
             : Problem("Generation Failed");
     }
